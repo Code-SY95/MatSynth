45c95ca2c7d6cfe514028c31cbfe4aa88ee33bcc
Giuseppe Vecchio <gvecchio@users.noreply.huggingface.co>
2024-01-11 22:58:09 +0000

    Update scripts/download_dataset.py

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -4,20 +4,29 @@ from pathlib import Path
 
 classlist = ["Blends", "Ceramic", "Concrete", "Fabric", "Leather", "Marble", "Metal", "Misc", "Plaster", "Plastic", "Stone", "Terracotta", "Wood"]
 
+def download_stream(url, dest_file):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(dest_file, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
 def download_dataset(base_dir, class_names=None):
-    dset_url = "https://huggingface.co/datasets/gvecchio/MatSynth/resolve/main/maps/"
+    dset_url = "https://huggingface.co/datasets/gvecchio/MatSynth/resolve/main/maps"
 
     classes = class_names.split(",") if class_names else classlist
     if classes:
         for split in ["train", "test"]:
+            dest_dir = base_dir/split
+            dest_dir.mkdir(parents=True, exist_ok=True)
+
             for class_name in classes:
-                r = requests.get(f"{dset_url}/{split}/{class_name}.zip")
-                with open(base_dir/split/(class_name + ".zip"), "wb") as f:
-                    f.write(r.content)
-                if class_name in ["Ground", "Wood"]:
-                    r = requests.get(dset_url + class_name + ".z01")
-                    with open(base_dir/split/(class_name + ".z01"), "wb") as f:
-                        f.write(r.content)
+                req = f"{dset_url}/{split}/{class_name}.zip"
+                download_stream(req, dest_dir/(class_name + ".zip"))
+
+                if class_name in ["Ground", "Wood"] and split == "train":
+                    req = f"{dset_url}/{split}/{class_name}.z01"
+                    download_stream(req, dest_dir/(class_name + ".z01"))
     
 if __name__ == "__main__":
     # Create argument parser
